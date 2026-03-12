26e94a994508520188e14435fd48c8979c5541f3
Your Name <you@example.com>
2026-03-12 12:30:21 +0530

    week8

---
+++ scripts/week8.R
@@ -0,0 +1,74 @@
+#load and understand mtcars dataset
+#load dataset
+data(mtcars)
+
+#inspect structure
+str(mtcars)
+
+#view first few rows
+head(mtcars)
+
+#Basic heat map(base r)
+heatmap(as.matrix(mtcars))
+
+#improved heat map with labels
+heatmap(as.matrix(mtcars),main="Heatmap of mtcars Datset")
+
+#Heat map with custom colors
+colors<-colorRampPalette(c("blue","yellow","red"))(50)
+heatmap(as.matrix(mtcars),col=colors,main="Custom Color Heatmap")
+
+#correlation heatmap(base R)
+corr_matrix<-cor(mtcars)
+heatmap(corr_matrix,col=colorRampPalette(c("blue","white","red"))(50),
+        main='Correlation heatmap of mtcars')
+
+#ggplot2 Heatmap(basic)
+install.packages('reshape2')
+library(reshape2)
+mtcars$Car<-rownames(mtcars)
+mtcars_long<-melt(mtcars,id.vars="Car")
+View(mtcars_long)
+library(ggplot2)
+ggplot(mtcars_long,aes(x=variable,y=Car,fill=value))+
+  geom_tile()
+
+#styled ggplot heatmap
+ggplot(mtcars_long,aes(x=variable,y=Car,fill=value))+
+  geom_tile()+
+  scale_fill_gradient(low="white",high="red")+
+  theme_minimal()+
+  labs(title="Heatmap of mtcars Dataset")
+
+#Another way
+ggplot(mtcars_long,aes(variable,Car,fill=value))+
+  geom_tile()+
+  scale_fill_viridis_c()+
+  theme_minimal()
+
+#correlation heatmap with ggplot2
+corr_matrix<-cor(mtcars[,sapply(mtcars,is.numeric)])
+corr_long<-melt(corr_matrix)
+ggplot(corr_long,aes(x=Var1,y=Var2,fill=value))+
+  geom_tile()+
+  scale_fill_gradient2( low="blue",mid="white",high="red",midpoint=0)+
+  theme_minimal()+
+  labs(title="Correlation heatmap of mtcars")
+
+#Heat Map with Values Displayed
+ggplot(corr_long,aes(x=Var1,y=Var2,fill=value)) +
+  geom_tile() +
+  geom_text(aes(label = round(value,2))) +
+  scale_fill_gradient2( low = "blue", mid = "white", high = "red", midpoint = 0) +
+  theme_minimal()
+
+
+#Advanced Heat Map using 'pheatmap'
+install.packages("pheatmap")
+library(pheatmap)
+pheatmap(
+  cor(mtcars[,sapply(mtcars,is.numeric)]),
+  display_numbers=round(corr_matrix,2),
+  main="Advanced Correlation Heatmap"
+)
+
